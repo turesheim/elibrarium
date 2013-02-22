df9ac1f1b50f04ed6082c0664b0b57ed2e65aceb
Torkild U. Resheim <torkildr@gmail.com>
2013-02-22 10:15:53 +0100

    Allow h2 to automatically start in server mode.

--- no.resheim.elibrarium.library.core/src/no/resheim/elibrarium/library/core/Librarian.java
+++ no.resheim.elibrarium.library.core/src/no/resheim/elibrarium/library/core/Librarian.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Torkild U. Resheim.
+ * Copyright (c) 2012, 2013 Torkild U. Resheim.
  * 
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
@@ -54,25 +54,33 @@ import org.h2.jdbcx.JdbcDataSource;
 import org.h2.tools.Server;
 import org.osgi.framework.BundleContext;
 
-/**
- * 
- * @author Torkild U. Resheim
- * 
- */
 public class Librarian extends Plugin implements ILibrarian {
+	/** Identifier of this plug-in */
+	public static final String PLUGIN_ID = "no.resheim.elibrarium.library.core";
+
+	/** The default CDO server port */ 
+	private static int serverPort = 2629;
+
+	/** Port of the HTTP based database console. */
+	public static final int CONSOLE_SERVER_PORT = getServerPort() + 1;
+
+	/** TCP address of the H2 server. */
+	public static final String DB_SERVER_ADDRESS = "127.0.0.1:" + getServerPort();
+
+	public static final OMBundle BUNDLE = OMPlatform.INSTANCE.bundle(PLUGIN_ID, Librarian.class);
 
 	/** Identifier of the CDO repository used */
 	public static final String CDO_REPOSITORY_ID = "elibrarium";
 
-	public static final String PLUGIN_ID = "no.resheim.elibrarium.library.core";
-
-	public static final OMBundle BUNDLE = OMPlatform.INSTANCE.bundle(PLUGIN_ID, Librarian.class);
+	private static IAcceptor acceptor;
 
 	public static final OMTracer DEBUG = BUNDLE.tracer("debug"); //$NON-NLS-1$
 
 	public static final OMLogger LOG = BUNDLE.logger();
 
 	private static Librarian plugin;
+
+	private static IRepository repository;
 
 	/**
 	 * Returns the shared instance
@@ -83,9 +91,15 @@ public class Librarian extends Plugin implements ILibrarian {
 		return plugin;
 	}
 
+	public static int getServerPort() {
+		return serverPort;
+	}
+
 	private final ArrayList<ICollection> collection;
 
 	private final ListenerList listeners;
+
+	private String storageLocation = null;
 
 	public Librarian() {
 		collection = new ArrayList<ICollection>();
@@ -127,6 +141,148 @@ public class Librarian extends Plugin implements ILibrarian {
 		}
 	}
 
+	public void addCollection(ICollection provider) {
+		collection.add(provider);
+		provider.addListener(this);
+	}
+
+	public void addListener(ILibraryListener listener) {
+		listeners.add(listener);
+	}
+
+	private void configure() {
+		File user = new File(System.getProperties().getProperty("user.dir") + File.separator + ".elibrarium");
+		File home = new File(System.getProperties().getProperty("user.home") + File.separator + ".elibrarium");
+		try {
+			if (user.exists()) {
+				readConfiguration(user);
+			} else if (home.exists()) {
+				readConfiguration(home);
+			}
+		} catch (IOException e) {
+		}
+		if (storageLocation == null) {
+			String root = System.getProperty("user.home");
+			String os = System.getProperty("os.name").toLowerCase();
+			if (os.indexOf("mac") > -1) {
+				root = root + File.separator + "Library" + File.separator;
+			}
+			storageLocation = new File(root + File.separator + "Elibrarium").getAbsolutePath();
+		}
+
+	}
+
+	/**
+	 * Start the h2 database server.
+	 * 
+	 * @throws Exception
+	 * @see {@link #getStorageLocation()}
+	 */
+	protected void doStart() throws Exception {
+		LOG.info("Elibrārium server starting");
+		JdbcDataSource dataSource = new JdbcDataSource();
+		// XXX: This is a workaround for not handling inter-process transactions
+		// through CDO. It also solves several other problems, such as what
+		// happens when the database server dies.
+		//
+		// http://h2database.com/html/features.html#auto_mixed_mode
+		String url = "jdbc:h2:" + getStorageLocation() + File.separator + "h2db;AUTO_SERVER=TRUE";
+		// String url = "jdbc:h2:" + getStorageLocation() + File.separator + "h2db";
+		LOG.info("- Hibernate database URL = " + url);
+		dataSource.setURL(url);
+
+		IMappingStrategy mappingStrategy = CDODBUtil.createHorizontalMappingStrategy(true);
+		IDBAdapter dbAdapter = new H2Adapter();
+		IDBConnectionProvider dbConnectionProvider = DBUtil.createConnectionProvider(dataSource);
+		IStore store = CDODBUtil.createStore(mappingStrategy, dbAdapter, dbConnectionProvider);
+
+		Map<String, String> props = new HashMap<String, String>();
+		props.put(IRepository.Props.OVERRIDE_UUID, CDO_REPOSITORY_ID);
+		props.put(IRepository.Props.SUPPORTING_AUDITS, "true");
+		props.put(IRepository.Props.SUPPORTING_BRANCHES, "false");
+
+		repository = CDOServerUtil.createRepository(CDO_REPOSITORY_ID, store, props);
+		CDOServerUtil.addRepository(IPluginContainer.INSTANCE, repository);
+		CDONet4jServerUtil.prepareContainer(IPluginContainer.INSTANCE);
+
+		acceptor = (IAcceptor) IPluginContainer.INSTANCE.getElement("org.eclipse.net4j.acceptors", "tcp",
+				DB_SERVER_ADDRESS);
+		startConsole();
+		LOG.info("Elibrārium server started");
+	}
+
+	/**
+	 * Stop the CDO persistence server.
+	 * 
+	 * @throws Exception
+	 */
+	protected void doStop() throws Exception {
+		LOG.info("Elibrārium stopping");
+		LifecycleUtil.deactivate(acceptor);
+		LifecycleUtil.deactivate(repository);
+		LOG.info("Elibrārium stopped");
+	}
+
+	/**
+	 * Returns the identified by the external identifier.
+	 * 
+	 * @param urn
+	 *            the book identifier
+	 * @return a list of books
+	 */
+	public synchronized Book getBookByURN(String urn) {
+		EList<Book> items = ILibraryCatalog.INSTANCE.getLibrary().getBooks();
+		for (Book item : items) {
+			if (item.getBookURN().equals(urn)) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns a list of all books found in the collection with the given
+	 * identifier.
+	 * 
+	 * @param id
+	 *            the collection identifier
+	 * @return a list of books
+	 */
+	public synchronized List<Book> getBooksByCollection(String id) {
+		ArrayList<Book> books = new ArrayList<Book>();
+		EList<Book> items = ILibraryCatalog.INSTANCE.getLibrary().getBooks();
+		for (Book item : items) {
+			if (item.getCollection().equals(id)) {
+				books.add(item);
+			}
+		}
+		return books;
+	}
+
+	public synchronized Library getLibrary() {
+		return ILibraryCatalog.INSTANCE.getLibrary();
+	}
+
+	/**
+	 * Returns the root folder of where Elibrarium stores it's data files. On OS
+	 * X this is <i>~/Library/Elibrarium</i>. On other platforms it is at
+	 * ~/Elibrarium.
+	 * 
+	 * @return the path to the storage location.
+	 * @throws IOException
+	 */
+	public IPath getStorageLocation() {
+		return new Path(storageLocation);
+
+	}
+
+	private void readConfiguration(File file) throws FileNotFoundException, IOException {
+		Properties p = new Properties();
+		p.load(new FileReader(file));
+		storageLocation = p.getProperty("storage.location");
+		serverPort = Integer.parseInt(p.getProperty("server.port"));
+	}
+
 	@Override
 	public synchronized void removeBook(final Book book) {
 		// Create a new transaction for modifying the library
@@ -159,54 +315,8 @@ public class Librarian extends Plugin implements ILibrarian {
 		}
 	}
 
-	public void addListener(ILibraryListener listener) {
-		listeners.add(listener);
-	}
-
 	public void removeListener(ILibraryListener listener) {
 		listeners.remove(listener);
-	}
-
-	/**
-	 * Returns the identified by the external identifier.
-	 * 
-	 * @param urn
-	 *            the book identifier
-	 * @return a list of books
-	 */
-	public synchronized Book getBookByURN(String urn) {
-		EList<Book> items = ILibraryCatalog.INSTANCE.getLibrary().getBooks();
-		for (Book item : items) {
-			if (item.getBookURN().equals(urn)) {
-				return item;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param id
-	 * @return
-	 */
-	public synchronized List<Book> getBooksByCollection(String id) {
-		ArrayList<Book> books = new ArrayList<Book>();
-		EList<Book> items = ILibraryCatalog.INSTANCE.getLibrary().getBooks();
-		for (Book item : items) {
-			if (item.getCollection().equals(id)) {
-				books.add(item);
-			}
-		}
-		return books;
-	}
-
-	public synchronized Library getLibrary() {
-		return ILibraryCatalog.INSTANCE.getLibrary();
-	}
-
-	public void addCollection(ICollection provider) {
-		collection.add(provider);
-		provider.addListener(this);
 	}
 
 	@Override
@@ -215,113 +325,6 @@ public class Librarian extends Plugin implements ILibrarian {
 		configure();
 		doStart();
 		CdoLibraryCatalog.INSTANCE.activate();
-	}
-
-	private void configure() {
-		File user = new File(System.getProperties().getProperty("user.dir") + File.separator + ".elibrarium");
-		File home = new File(System.getProperties().getProperty("user.home") + File.separator + ".elibrarium");
-		try {
-		if (user.exists()) {
-			readConfiguration(user);
-		} else if (home.exists()) {
-			readConfiguration(home);
-		}
-		} catch (IOException e) {
-		}
-		if (storageLocation == null) {
-			String root = System.getProperty("user.home");
-			String os = System.getProperty("os.name").toLowerCase();
-			if (os.indexOf("mac") > -1) {
-				root = root + File.separator + "Library" + File.separator;
-			}
-			storageLocation = new File(root + File.separator + "Elibrarium").getAbsolutePath();
-		}
-
-	}
-
-	private void readConfiguration(File file) throws FileNotFoundException, IOException {
-		Properties p = new Properties();
-		p.load(new FileReader(file));
-		storageLocation = p.getProperty("storage.location");
-		serverPort = Integer.parseInt(p.getProperty("server.port"));
-	}
-
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		doStop();
-		CdoLibraryCatalog.INSTANCE.deactivate();
-	}
-
-	/**
-	 * Returns the root folder of where Elibrarium stores it's data files. On OS
-	 * X this is <i>~/Library/Elibrarium</i>. On other platforms it is at
-	 * ~/Elibrarium.
-	 * 
-	 * @return the path to the storage location.
-	 * @throws IOException
-	 */
-	public IPath getStorageLocation() {
-		return new Path(storageLocation);
-
-	}
-
-	private static int serverPort = 2629;
-
-	/**
-	 * Port of the HTTP based database console.
-	 */
-	public static final int CONSOLE_SERVER_PORT = getServerPort() + 1;
-
-	/**
-	 * TCP address of the H2 server.
-	 */
-	public static final String DB_SERVER_ADDRESS = "127.0.0.1:" + getServerPort();
-
-	private static IAcceptor acceptor;
-
-	private static IRepository repository;
-
-	private String storageLocation = null;
-
-	/**
-	 * Start the h2 database server.
-	 * 
-	 * @throws Exception
-	 * @see {@link #getStorageLocation()}
-	 */
-	protected void doStart() throws Exception {
-		LOG.info("Elibrārium server starting");
-		JdbcDataSource dataSource = new JdbcDataSource();
-		// This is a workaround for not handling inter-process transactions
-		// through CDO. It also solves several other problems, such as what
-		// happens when the database server dies.
-		//
-		// http://h2database.com/html/features.html#auto_mixed_mode
-		// String url = "jdbc:h2:" + getStorageLocation() + File.separator +
-		// "h2db;AUTO_SERVER=TRUE";
-		String url = "jdbc:h2:" + getStorageLocation() + File.separator + "h2db";
-		LOG.info("- Hibernate database URL = " + url);
-		dataSource.setURL(url);
-
-		IMappingStrategy mappingStrategy = CDODBUtil.createHorizontalMappingStrategy(true);
-		IDBAdapter dbAdapter = new H2Adapter();
-		IDBConnectionProvider dbConnectionProvider = DBUtil.createConnectionProvider(dataSource);
-		IStore store = CDODBUtil.createStore(mappingStrategy, dbAdapter, dbConnectionProvider);
-
-		Map<String, String> props = new HashMap<String, String>();
-		props.put(IRepository.Props.OVERRIDE_UUID, CDO_REPOSITORY_ID);
-		props.put(IRepository.Props.SUPPORTING_AUDITS, "true");
-		props.put(IRepository.Props.SUPPORTING_BRANCHES, "false");
-
-		repository = CDOServerUtil.createRepository(CDO_REPOSITORY_ID, store, props);
-		CDOServerUtil.addRepository(IPluginContainer.INSTANCE, repository);
-		CDONet4jServerUtil.prepareContainer(IPluginContainer.INSTANCE);
-
-		acceptor = (IAcceptor) IPluginContainer.INSTANCE.getElement("org.eclipse.net4j.acceptors", "tcp",
-				DB_SERVER_ADDRESS);
-		startConsole();
-		LOG.info("Elibrārium server started");
 	}
 
 	/**
@@ -337,19 +340,10 @@ public class Librarian extends Plugin implements ILibrarian {
 		}
 	}
 
-	/**
-	 * Stop the CDO persistence server.
-	 * 
-	 * @throws Exception
-	 */
-	protected void doStop() throws Exception {
-		LOG.info("Elibrārium stopping");
-		LifecycleUtil.deactivate(acceptor);
-		LifecycleUtil.deactivate(repository);
-		LOG.info("Elibrārium stopped");
-	}
-
-	public static int getServerPort() {
-		return serverPort;
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		doStop();
+		CdoLibraryCatalog.INSTANCE.deactivate();
 	}
 }
