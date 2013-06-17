0819d76ba2b340f63351ae84705b746bbec213f8
Torkild U. Resheim <torkildr@gmail.com>
2013-06-17 23:29:53 +0200

    Fixed issue #40: Make sure tests are executed during Maven/Tycho build

--- no.resheim.elibrarium.library.tests/src/no/resheim/elibrarium/library/tests/CdoLibraryCatalogTest.java
+++ no.resheim.elibrarium.library.tests/src/no/resheim/elibrarium/library/tests/CdoLibraryCatalogTest.java
@@ -1,15 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2013 Torkild U. Resheim.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Torkild U. Resheim - initial API and implementation
+ *******************************************************************************/
 package no.resheim.elibrarium.library.tests;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.FileWriter;
 
-import junit.framework.TestCase;
 import no.resheim.elibrarium.library.Book;
 import no.resheim.elibrarium.library.LibraryFactory;
 import no.resheim.elibrarium.library.core.CdoLibraryCatalog;
 import no.resheim.elibrarium.library.core.Librarian;
 
 import org.eclipse.emf.common.util.EList;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -17,27 +31,26 @@ import org.junit.Test;
  * API.
  * 
  * @author Torkild U. Resheim
- * @since 1.0
  */
-public class CdoLibraryCatalogTest extends TestCase {
+public class CdoLibraryCatalogTest {
 	/** A unique identifier for the test book */
 	private static final String BOOK_ID = "urn:uuid:5c7cb470-fccc-11e1-a21f-0800200c9a66";
 
 	private static final String CONFIG_FILE = System.getProperties().getProperty("user.dir") + File.separator
 			+ ".elibrarium";
 
-	/** Port to use for the CDO server */
+	/** Port to use for the CDO server when testing */
 	private static final int SERVER_PORT = 1969;
 
-	private File configurationFile;
+	private static File configurationFile;
 
-	private File storageLocation;
+	private static File storageLocation;
 
-	private boolean deleteFolder(File folder) {
+	private static boolean deleteFolder(File folder) {
 		if (folder.isDirectory()) {
 			String[] children = folder.list();
-			for (int i = 0; i < children.length; i++) {
-				boolean ok = deleteFolder(new File(folder, children[i]));
+			for (String element : children) {
+				boolean ok = deleteFolder(new File(folder, element));
 				if (!ok) {
 					return false;
 				}
@@ -46,19 +59,19 @@ public class CdoLibraryCatalogTest extends TestCase {
 		return folder.delete();
 	}
 
-	@Override
-	protected void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
+		System.out.println("Writing configuration file for Elibrarium test server.");
 		configurationFile = new File(CONFIG_FILE);
 		FileWriter fw = new FileWriter(configurationFile);
 		storageLocation = new File(System.getProperties().getProperty("user.dir") + File.separator + "test");
 		fw.write("storage.location=" + storageLocation.getAbsolutePath() + "\n");
 		fw.write("server.port=" + SERVER_PORT + "\n");
 		fw.close();
-		super.setUp();
 	}
 
-	@Override
-	protected void tearDown() throws Exception {
+	@AfterClass
+	public static void tearDown() throws Exception {
 		// Delete the configuration file
 		if (configurationFile.exists()) {
 			configurationFile.delete();
